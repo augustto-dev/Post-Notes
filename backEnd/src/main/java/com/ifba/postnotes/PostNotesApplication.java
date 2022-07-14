06b0a7bf352dad5cf0e0f2c6ad7d4613ab69b905
augustto-dev <contatoaugustogomespessoal@outlook.com>
2022-07-14 18:55:59 -0300

    Fazendo primeiro commit do projeto

---
+++ backEnd/src/main/java/com/ifba/postnotes/PostNotesApplication.java
@@ -0,0 +1,13 @@
+package com.ifba.postnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostNotesApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/com/ifba/postnotes/PostNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifba.postnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
